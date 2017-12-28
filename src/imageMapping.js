8904dbaa0573a0955fe440e6a246cadab2557966
Evan Dana <evan.dana@gmail.com>
2017-12-28 13:07:41 -0500

    Adding specs, images, and markup for these (#17)

---
+++ src/imageMapping.js
@@ -0,0 +1,71 @@
+/** IMAGES **/
+import img_01_SudsNShine from './static/images/products/01_SudsNShine.jpg';
+import img_02_WipeOut from './static/images/products/02_WipeOut.jpg';
+import img_03_StreakFreeRTU from './static/images/products/03_StreakFreeRTU.jpg'
+import img_03__WaterlessWashAndWax from './static/images/products/03__WaterlessWashAndWax.jpg'
+import img_04_EcoKleen from './static/images/products/04_EcoKleen.jpg'
+import img_05_FoamCitrusSurface from './static/images/products/05_FoamCitrusSurface.jpg'
+import img_06_Resurrect from './static/images/products/06_Resurrect.jpg'
+import img_07_10inchTruckBrush from './static/images/products/07_10inchTruckBrush.jpg'
+import img_08_WashMitt from './static/images/products/08_WashMitt.jpg'
+import img_09_TriggerSprayer from './static/images/products/09_TriggerSprayer.jpg'
+import img_12_SnowRake from './static/images/products/12_SnowRake.jpg'
+import img_13_TorkTowels from './static/images/products/13_TorkTowels.jpg'
+import img_13a_TorkDispenser from './static/images/products/13a_TorkDispenser.jpg'
+import img_13b_TorkTowels from './static/images/products/13b_TorkTowels.jpg'
+import img_14_Motor from './static/images/products/14_Motor.jpg'
+import img_OdorMedic from './static/images/products/OdorMedic.jpg'
+import img__DoubleDazzle from './static/images/products/_DoubleDazzle.jpg'
+import img__MF1MicoFiberTowels from './static/images/products/_MF1MicoFiberTowels.jpg'
+import img__N903NitrileGloves from './static/images/products/_N903NitrileGloves.jpg'
+import img__NWP310 from './static/images/products/_NWP310.jpg'
+import img__Picture023 from './static/images/products/_Picture023.jpg'
+import img__VH20VacHose from './static/images/products/_VH20VacHose.jpg'
+import img__VV2 from './static/images/products/_VV2.jpg'
+import img__WasherFluid110153 from './static/images/products/_WasherFluid110153.jpg'
+import img_corner from './static/images/products/corner.jpg'
+import img_lbdb from './static/images/products/lbdb.jpg'
+import img_lbdb2 from './static/images/products/lbdb2.jpg'
+import img_lgdb from './static/images/products/lgdb.jpg'
+import img_lgdb2 from './static/images/products/lgdb2.jpg'
+import img_lglb from './static/images/products/lglb.jpg'
+import img_testimage1 from './static/images/products/testimage1.jpg'
+import img_wellworth_logo from './static/images/products/wellworth_logo.jpg'
+
+/** IMAGES  **/
+const IMAGE_MAPPING = {
+    '01_SudsNShine.jpg': img_01_SudsNShine,
+    '02_WipeOut.jpg': img_02_WipeOut,
+    '03_StreakFreeRTU.jpg': img_03_StreakFreeRTU,
+    '03__WaterlessWashAndWax.jpg': img_03__WaterlessWashAndWax,
+    '04_EcoKleen.jpg': img_04_EcoKleen,
+    '05_FoamCitrusSurface.jpg': img_05_FoamCitrusSurface,
+    '06_Resurrect.jpg': img_06_Resurrect,
+    '07_10inchTruckBrush.jpg': img_07_10inchTruckBrush,
+    '08_WashMitt.jpg': img_08_WashMitt,
+    '09_TriggerSprayer.jpg': img_09_TriggerSprayer,
+    '12_SnowRake.jpg': img_12_SnowRake,
+    '13_TorkTowels.jpg': img_13_TorkTowels,
+    '13a_TorkDispenser.jpg': img_13a_TorkDispenser,
+    '13b_TorkTowels.jpg': img_13b_TorkTowels,
+    '14_Motor.jpg': img_14_Motor,
+    'OdorMedic.jpg': img_OdorMedic,
+    '_DoubleDazzle.jpg': img__DoubleDazzle,
+    '_MF1MicoFiberTowels.jpg': img__MF1MicoFiberTowels,
+    '_N903NitrileGloves.jpg': img__N903NitrileGloves,
+    '_NWP310.jpg': img__NWP310,
+    '_Picture023.jpg': img__Picture023,
+    '_VH20VacHose.jpg': img__VH20VacHose,
+    '_VV2.jpg': img__VV2,
+    '_WasherFluid110153.jpg': img__WasherFluid110153,
+    'corner.jpg': img_corner,
+    'lbdb.jpg': img_lbdb,
+    'lbdb2.jpg': img_lbdb2,
+    'lgdb.jpg': img_lgdb,
+    'lgdb2.jpg': img_lgdb2,
+    'lglb.jpg': img_lglb,
+    'testimage1.jpg': img_testimage1,
+    'wellworth_logo.jpg': img_wellworth_logo,
+};
+
+export default IMAGE_MAPPING;
