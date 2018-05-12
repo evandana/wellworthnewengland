a381c2aed499d6fd98c9f11b912b111bddf22eca
Evan Dana <evan.dana@gmail.com>
2018-05-12 08:23:37 -0400

    Revert "Email Seller and Customer when Order Placed (#15)"
    This reverts commit 2a8ef52147b9db983cef2557fe8a5404dac37364.

--- functions/README.md
+++ functions/README.md
@@ -3,18 +3,17 @@
 
 ## Deploy
 
-1. *Set up environment variables*: Follow instructions in `**/function-secrets.md`
+1. *Set up environment variables*: Follow instructions in `/notes/secrets.md`
 1. *Start the server* `firebase deploy --only functions`
 
 
 ## Run Locally
 
-1. `npm install -g firebase-tools`
-1. *Set up environment variables*: Follow instructions in `**/function-secrets.md`
+1. *Set up environment variables*: Follow instructions in `/notes/secrets.md`
 1. `npm install`
 1. `node index.js`
 
 
 ## Setting Up Private Keys
 
-Go to [Service Account](https://console.firebase.google.com/u/0/project/wellworthnewengland-1e81a/settings/serviceaccounts/adminsdk) page and click `Generate New Private Key`
+Go to [Service Account](https://console.firebase.google.com/u/0/project/_/settings/serviceaccounts/adminsdk) page and click `Generate New Private Key`
--- functions/index.js
+++ functions/index.js
@@ -17,62 +17,159 @@
 
 
 
+// [START imports]
 var firebase = require('firebase-admin');
+// [END imports]
 var nodemailer = require('nodemailer');
+// var schedule = require('node-schedule');
+var Promise = require('promise');
+// var escape = require('escape-html');
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const util = require('util');
+
+var whitelist = ['http://wellworthnewengland.com'];
+// const cors = require('cors')({origin: true});
+// const cors = require('cors')({origin: 'http://wellworthnewengland.com'});
+const cors = require('cors')({
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      console.log('cors allowed');
+      callback(null, true)
+    } else {
+      console.log('cors not allowed');
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+});
 
 // Import the Firebase SDK for Google Cloud Functions.
 const functions = require('firebase-functions');
+// Import and initialize the Firebase Admin SDK.
 
+
+var serviceAccount = require('./zellner-email-firebase-adminsdk-2ool3-7ba626a85d.json');
+const admin = require('firebase-admin');
+
+// Configure the email transport using the default SMTP transport and a GMail account.
+// See: https://nodemailer.com/
+// For other types of transports (Amazon SES, Sendgrid...) see https://nodemailer.com/2-0-0-beta/setup-transporter/
 var mailTransport = nodemailer.createTransport(`smtps://${functions.config().email.account}%40gmail.com:${functions.config().email.pw}@smtp.gmail.com`);
 
-exports.ordercreated = functions.database.ref('/orders/{orderId}')
-  .onCreate(event => {
-    // Grab the current value of what was written to the Realtime Database.
-    const newOrder = event.data.val();
-    var emailRecipients = [`${functions.config().email.account}@gmail.com`, newOrder.customerInfo.email];
+// [START initialize]
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: firebase.credential.cert(serviceAccount),
+  databaseURL: 'https://zellner-email.firebaseio.com'
+});
+// admin.initializeApp(functions.config().firebase);
+// [END initialize]
 
-    console.log('received', JSON.stringify(newOrder));
-    console.log('email recipients: ', emailRecipients);
-    console.log('email disabled for now');
 
-    // TODO: enable once ready
-    // sendEmail([emailRecipients, newOrder.customerInfo.email], createEmailHtml(newOrder));
+exports.placeorder = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    if (validateRequest(req)) {
 
-    return;
+      console.log('req body', req.body);
+
+      // remove unnecessary props not intended to be in the email
+      delete req.body.info;
+      delete req.body.submit;
+
+      var emailRecipients = [{ email: `${functions.config().email.account}@gmail.com` }];
+      sendEmail(emailRecipients, 'ORDER ITEMS: ' + JSON.stringify(req.body));
+
+      console.log('email obj', emailRecipients);
+
+      res.status(200).send('sending email to ' + `${functions.config().email.account}@gmail.com` + ' with contents: ' + JSON.stringify(req.body));
+    } else {
+
+      // do nothing if origin is not correct
+      res.status(500).send('invalid origin');
+    }
+  })
+
+
+});
+
+function validateRequest(req) {
+
+  console.log('headers', 'req', req.headers);
+
+  if (req.method === "POST" &&
+    (req.headers.origin.substr(0, 'http://wellworthnewengland.com'.length) === 'http://wellworthnewengland.com' || req.headers.origin === 'chrome-extension://fhbjgbiflinjbdggehcddcbncdddomop')
+  ) {
+
+    // if ( validateHash(req.headers.wellworth) ) {
+    return true;
+
+  } else {
+    // origin not allowed
+    return false;
   }
-);
 
-function createEmailHtml(order) {
-  var emailStr = '';
-  emailStr += 'ORDER FROM ZELLNER DISTRIBUTING WEBSITE' + '<br/><br/>';
-  emailStr += 'FROM: ' + order.customerInfo.name + ' ('+order.customerInfo.name+')' + '<br/><br/>';
-  emailStr += 'TIME: ' + new Date(order.timestamp) + ')' + '<br/><br/>';
-  emailStr += 'TOTAL: $' + order.total/100 + '<br/>';
-  emailStr += 'ITEMS: ' + '<br/>';
-
-  order.items.forEach(item => {
-    emailStr += item.quantity + ' x ' + item.name + ' ( ' + item.optionSize + ' @ $ ' + item.optionPrice/100 + ' ) ' + '<br/>';
-  });
-
-  return emailStr;
 }
+
+function generateHash() {
+  var t = new Date();
+  t = t.getTime();
+  return encodeNum(t);
+}
+
+function encodeNum(n) {
+  var c = 'abcdefghijklmnopqrstuvwxyz';
+  var nArray = n.toString().split('');
+  var cArray = [];
+  var offset = 0;
+  for (var i = 0; i < nArray.length; i++) {
+    cArray.push(c[parseInt(nArray[i], 10) + offset]);
+  }
+  return c[offset] + cArray.join('');
+}
+
+
+function decodeNum(c) {
+  var a = 'abcdefghijklmnopqrstuvwxyz';
+  var cArray = c.split('');
+  var nArray = [];
+  var offset = a.indexOf(cArray.shift());
+  for (var i = 0; i < cArray.length; i++) {
+    nArray.push(a.indexOf(cArray[i]) - offset);
+  }
+  return parseInt(nArray.join(''), 10);
+}
+
+function validateHash(hash) {
+
+  var decodedNum = decodeNum(hash);
+
+  var now = new Date();
+  now = now.getTime();
+
+  var valid = decodedNum > now - 10 * 1000 && decodedNum < now + 10 * 1000;
+
+  return valid;
+}
+
 
 /*
  * Send email
  */
 function sendEmail(users, emailHtml) {
   console.log('start test email function');
-  users.forEach(function (email) {
-    if (email) {
+  Object.keys(users).forEach(function (uid) {
+    var user = users[uid];
+    if (user.email) {
       var mailOptions = {
         from: '"WellWorthNewEngland Email Server" <noreply@firebase.com>',
-        to: email,
+        to: user.email,
         subject: 'New Order from WellWorthNewEngland.com',
         html: emailHtml
       };
       console.log('sending mail');
       mailTransport.sendMail(mailOptions).then(function () {
-        console.log('Order email sent to: ' + email);
+        console.log('Order email sent to: ' + user.email);
         // Save the date at which we sent the weekly email.
         // [START basic_write]
         // return firebase.database().child('/users/' + uid + '/lastSentWeeklyTimestamp')
