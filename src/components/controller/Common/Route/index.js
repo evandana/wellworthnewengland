0ba0b254262dbe7e312b6662c9c812cb45c68ce8
Evan Dana <evan.dana@gmail.com>
2018-05-12 08:23:37 -0400

    Revert "Google Authentication and tons of goodies (#3)"
    This reverts commit a7997c2983c9f35523de6cd3b0c925ec46ff0228.

--- src/components/controller/Common/Route/index.js
+++
@@ -1,7 +0,0 @@
-import { connect } from 'react-redux';
-import AuthorizedRoute from '../../../view/Common/Route';
-
-const mapStateToProps = (state) => ({isLoggedIn: !!state.user});
-const AuthorizedRouteController = connect(mapStateToProps)(AuthorizedRoute);
-
-export default AuthorizedRouteController;
