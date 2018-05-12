e9ff63643e0ed815833666c1860e4eb4cee10e9b
Evan Dana <evan.dana@gmail.com>
2018-05-12 08:23:37 -0400

    Revert "Google Authentication and tons of goodies (#3)"
    This reverts commit a7997c2983c9f35523de6cd3b0c925ec46ff0228.

--- src/components/controller/Common/Modal/Login/index.js
+++
@@ -1,9 +0,0 @@
-import { connect } from 'react-redux';
-import { loginGoogleRequest } from 'actions';
-import LoginModal from 'components/view/Common/Modal/Login';
-
-const LoginModalController = connect(
-    null, { loginGoogleRequest }
-)(LoginModal);
-
-export default LoginModalController;
--- src/components/view/Authorized/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const AuthorizedPage = props => {
-    return (
-        <div className="page">This is an Authorized Page</div>
-    );
-};
-
-export default AuthorizedPage;
